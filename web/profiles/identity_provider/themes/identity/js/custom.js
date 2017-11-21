1e194614b83e9bb6e356eb4fd8f4a61ed21941bb
Stefan Auditor <stefan.auditor@erdfisch.de>
2017-11-21 10:27:30 +0100

    Rename theme identityprovider to identity

--- web/profiles/identity_provider/themes/identity/js/custom.js
+++ web/profiles/identity_provider/themes/identity/js/custom.js
@@ -1,13 +1,7 @@
-// Using the closure to map jQuery to $.
 (function ($) {
 
-
-
-
-// Store our function as a property of Drupal.behaviors.
-  Drupal.behaviors.identityprovider = {
+  Drupal.behaviors.identity = {
     attach: function (context, settings) {
-
       $('.menu-dropdown-toggle > a > span').not('.rippleWrap').html(
         $('#language-dropdown li.is-active a').text()
       );
